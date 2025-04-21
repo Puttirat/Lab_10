906a4fb32da13fab16cf472cebb91ffb9fa44d5a
Puttirat <puttirat.lima@bumail.net>
2025-04-21 23:11:19 +0700

    Add scripts walk and jump

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,15 +2,31 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody2D rb2d;
+    
+    //walk
+    float move;
+    [SerializeField] float speed;
+    
+    //Jump
+    [SerializeField]float jumpForce;
+    
+    
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
-
-    // Update is called once per frame
     void Update()
     {
+        //walk
+        move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
         
+        //Jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb2d.AddForce(new Vector2 (rb2d.linearVelocity.x, jumpForce ) );
+            Debug.Log("Jump!"); //for debugging
+        }
     }
 }
